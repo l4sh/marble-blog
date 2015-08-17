0eba5583cf806272bd3575ac26776efd39483ac3
l4sh <l4sh.dev@gmail.com>
2015-08-17 00:18:54 -0430

    Fixed syntax & added exports

--- config.js
+++ config.js
@@ -1,25 +1,27 @@
-config = {
-  "author": {
-    "name": "MARBLE Author",
-    "description": "I post things, a lot of things",
-    "twitter_handle": "@my_twitter_handle",
-    "twitter": "https://twitter.com/twitter"
+var config = {
+  author: {
+    name: 'MARBLE Author',
+    description: 'I post things, a lot of things',
+    twitterHandle: '@my_twitter_handle',
+    twitter: 'https://twitter.com/twitter'
   },
 
-  "blog": {
-    "title": "My MARBLE blog",
-    "subtitle": "A MARkdown dynamic BLog Engine",
-    "description": "This is a blog running MARBLE, if you are the author of this blog you might want to update this information.",
-    "url": "https://github.com/l4sh/marble-blog",
-    "lang": "en",
-    "postsPerPage": 5,
-    "pagerPosition": "bottom",
-    "postsJSON": "./posts.json",
-    "excerptLength": 50,
-    "postsPath": "posts/",
-    "draftsFolder": "drafts",
-    "publishedFolder": "published"
+  blog: {
+    title: 'My MARBLE blog',
+    subtitle: 'A MARkdown dynamic BLog Engine',
+    description: 'This is a blog running MARBLE, if you are the author of this blog you might want to update this information.',
+    url: 'https://github.com/l4sh/marble-blog',
+    lang: 'en',
+    postsPerPage: 5,
+    pagerPosition: 'bottom',
+    postsJSON: './posts.json',
+    excerptLength: 50,
+    postsPath: 'posts/',
+    draftsFolder: 'drafts',
+    publishedFolder: 'published'
   },
 
-  "editor": "vim"
-}
+  editor: 'vim'
+};
+
+module.exports = config

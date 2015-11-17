e40fd29dddab9501c297edff37652c0547a1bffa
Luis Salazar (l4sh) <l4sh.dev@gmail.com>
2015-11-16 23:36:18 -0430

    Author information plugin

--- plugins/author_info/main.js
+++ plugins/author_info/main.js
@@ -1,40 +1,48 @@
-/*
-Author info plugin for MARBLE
-
-It provides an author information widget
-
-This plugin needs jQuery, Bootstrap and the resources plugin
-*/
+// Author info plugin for MARBLE
+// It provides an author information widget
 
 //** Set media links **//
-function authorInfoMediaLinks() {
+function getMediaLinks() {
   var _s = false;
   var mediaLinks;
 
   for (var i in config.author) {
-    if (icons.webIcons[i]) {
+    if (resources.icons.web[i]) {
       if (!_s) {
         mediaLinks = '<ul class="list-inline">';
         _s = true;
       }
-      mediaLinks += '<li><a href="' + config.author[i] + '">';
-      mediaLinks += '<i class="fa ' + resources.icons.web[i] + '"></i></a></li>';
+
+      var url = config.author[i];
+      var icon = resources.icons.web[i];
+      if (i === 'email') {
+        url= 'mailto://' + url;
+      }
+
+      mediaLinks += '<li><a href="' + url + '">';
+      mediaLinks += '<i class="fa ' + icon + '"></i></a></li>';
     }
   }
   if (mediaLinks) {
-    mediaLinks += '</ul>';
-
-    $('.media-links').html(mediaLinks);
+    return mediaLinks += '</ul>';
   }
 }
 
-function loadAuthorInfo(area) {
+function setAuthorInfo() {
+  var mediaLinks = getMediaLinks() || '';
 
+  var authorDiv = '<div id="author-info" class="row">';
+  authorDiv += '<div class="col-md-12 text-center">';
+  authorDiv += '<img class="img-circle center-block author-image" src="/images/blog/author.png">';
+  authorDiv += '<h3 class="author-name">' + config.author.name + '</h3>';
+  authorDiv += '<div class="media-links">' + mediaLinks + '</div>';
+  authorDiv += '<p class="author-description">' + config.author.description + '</p>';
+  authorDiv += '</div></div>';
+
+  $(config.plugins.authorInfo.area).append(authorDiv);
 }
 
 
-plugin.authorInfo = {
-  load: function(){
-
-  }
-};
+$(function(){
+  setAuthorInfo();
+});
